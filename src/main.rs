26b24ed48c99b0220a6765da46f46d243da3b018
Leon Fuß <leon.fuss0803@gmail.com>
2022-03-17 20:27:42 +0100

    added example

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,16 @@
-fn main() {}
+use actix_web::{web, App, HttpServer};
+
+async fn greet() -> String {
+    "Hello there".to_owned()
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}

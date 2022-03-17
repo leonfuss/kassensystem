b77214b0b252f10dd40aae45a0f4dc235b4a1a62
Leon Fuß <leon.fuss0803@gmail.com>
2022-03-17 21:29:36 +0100

    setup tests

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,6 @@
-use actix_web::{web, App, HttpServer};
-
-async fn greet() -> String {
-    "Hello there".to_owned()
-}
+use ausgleichende_gerechtigkeit::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    run("127.0.0.1:8000")?.await
 }
